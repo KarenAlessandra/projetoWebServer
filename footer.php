0010758591f4af511720441e1a0a0f4af7a07126
KarenAlessandra <20971247+KarenAlessandra@users.noreply.github.com>
2023-04-15 19:00:02 -0300

    up html & php

---
+++ footer.php
@@ -0,0 +1,11 @@
+<!doctype html>
+<html lang=en>
+
+<!-- footer -->
+<foot>
+    <div class="jumbotron2 jumbotron-fluid">
+        <img src="./assets/logo_footer.png"></img>
+    </div>
+</foot>
+
+</html>
--- index.php
+++ index.php
@@ -1,85 +1,22 @@
+<!-- pra abrir com o http-server-->
 <!-- ./node_modules/.bin/http-server -a localhost -p 8000 -c-1 -->
 
 <!doctype html>
 <html lang="en">
 
-<?php
-?>
-
 <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-
-    <!-- page's name -->
-    <title>Loja de Bolo</title>
-
-    <!-- imports-->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3"
-        crossorigin="anonymous"></script>
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
-        integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"
-        integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN"
-        crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"
-        integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q"
-        crossorigin="anonymous"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"
-        integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl"
-        crossorigin="anonymous"></script>
-
-    <!-- linkando os estilos do css.style ao html -->
-    <link href="./style.css" rel="stylesheet">
+    <?php
+    //incluir header da página
+    include("header.php");
+    ?>
 </head>
 
 <!-- page -->
 
 <body>
     <main>
-        <!-- icons at the header -->
-        <div class="container0">
-            <div class="row">
-                <div class="d-flex flex-row-reverse">
-                    <a href="./login.html" class="btn_icon"><i class="fa fa-user"></i></a>
-                    <ul class="dropdown-menu">
-                        <li class="dropdown-item">
-                            <a href="#perfil" class="btn_icon"><i class="fa fa-user"></i></a>
-                        </li>
-                        <li class="dropdown-item">
-                            <a href="#cadastrarBolo" class="btn_icon"><i class="fa fa-birthday-cake"></i></a>
-                        </li>
-                        <li class="dropdown-item">
-                            <a href="#deslogar" class="btn_icon"><i class="fa fa-sign-out"></i></a>
-                        </li>
-                    </ul>
-                </div>
-                &nbsp; &nbsp; &nbsp;
-                <!-- <button type="#cart" class="btn_carrinho" data-bs-toggle="modal"
-                    data-bs-target="#carrinho_de_compras">
-                    <span class="fa fa-shopping-cart" aria-hidden="true"></span>
-                </button> -->
-            </div>
-        </div>
-        </div>
-
+        <!-- Home -->
         <div class="containerPage">
-            <!-- links on navbar -->
-            <div class="container1">
-                <div class="row row-cols-5 text-center">
-                    <a href="./" class="btn_header">Home</a>
-                    <a href="./produtos.html" class="btn_header">Produtos</a>
-                    <img src="./assets/logo.png"></img>
-                    <a href="./quemsomos.html" class="btn_header">Quem Somos</a>
-                    <a href="./contato.html" class="btn_header">Contato</a>
-                </div>
-            </div>
-            <hr />
-
-            <!-- Home -->
             <br />
             <!-- <marquee scrollamount="30">
                 <h1 class="h1_title text-center" style="color: #d87f81;">Bem
@@ -169,14 +106,15 @@
             </div>
             <br /><br /><br />
         </div>
-
-        <!-- footer -->
-
-        <div class="jumbotron2 jumbotron-fluid">
-            <div class="container">
-                <img src="./assets/logo_footer.png"></img>
-            </div>
         </div>
+
+        <foot>
+            <?php
+            //incluir footer da página
+            include("footer.php");
+            ?>
+        </foot>
+
 
     </main>
     </div>
