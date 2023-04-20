0ec372cf3fc9a370ff760e8a1b4ff5a549801838
KarenAlessandra <20971247+KarenAlessandra@users.noreply.github.com>
2023-04-20 20:12:07 -0300

    checkout page

---
+++ checkout.php
@@ -0,0 +1,42 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <?php
+    include("header.php");
+    ?>
+</head>
+
+<body>
+    <main>
+        <br /><br />
+        <div class="container">
+            <div class="card text-center"
+                style="padding-top: 4rem; padding-left: 7rem; padding-right: 7rem; padding-bottom: 3rem; border-radius: 1%;">
+                <div class="row">
+                    <h1 class="h1_title" style="color: #f3dbcf;">Check Out</h1>
+                </div>
+
+                <hr />
+                <br /><br />
+                <p style="color: #f3dbcf">Seu carrinho está vazio!</p>
+
+                </form>
+            </div>
+        </div>
+        </div>
+
+        <br /><br /><br />
+
+        <foot>
+            <?php
+            include("footer.php");
+            ?>
+        </foot>
+
+        </div>
+
+    </main>
+</body>
+
+</html>
