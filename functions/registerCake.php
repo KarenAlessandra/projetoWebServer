1564ebd95da09c2b21a6d019d0c135da46d0f537
KarenAlessandra <20971247+KarenAlessandra@users.noreply.github.com>
2023-04-19 21:19:00 -0300

    end of php fixes, horray

--- functions/registerCake.php
+++ functions/registerCake.php
@@ -1,69 +1,35 @@
 <?php
+
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    if (isset($_POST['submit'])) {
+        $name = trim($_POST['name'], FILTER_SANITIZE_STRING);
+        $email = trim($_POST['email'], FILTER_SANITIZE_EMAIL);
+        $subject = trim($_POST['subject'], FILTER_SANITIZE_STRING);
+        $message = trim($_POST['message']);
 
-
-    if (empty($_FILES["File"]["name"])) {
-        $image_err = "Por favor, insira uma imagem.";
-    } else {
-        $target_dir = "uploads/";
-        $target_file = $target_dir . basename($_FILES["File"]["name"]);
-        $imageFileType = strtolower(pathinfo($target_file, PATHINFO_EXTENSION));
-        $allowed_types = array("jpg", "jpeg", "png");
-        if (!in_array($imageFileType, $allowed_types)) {
-            $image_err = "Somente arquivos JPG, JPEG e PNG são permitidos.";
-        } else {
-            move_uploaded_file($_FILES["File"]["tmp_name"], $target_file);
-            $image = $target_file;
-        }
-
-        if (empty($_POST["Price"])) {
+        if (empty($name) || empty($email) || empty($subject) || empty($message)) {
+            $error = "Por favor, preencha todos os campos do formulário.";
+            echo $error;
+        } else if (empty($_FILES["File"]["name"])) {
+            $error = "Por favor, insira uma imagem.";
+            echo $error;
+        } else if (empty($_POST["Price"])) {
             $price_err = "Por favor, insira o valor do bolo.";
-        } else {
-            $price = test_input($_POST["Price"]);
-            if (!preg_match("/^\d+(\.\d{2})?$/", $price)) {
-                $price_err = "O valor deve ser um número no formato xx,xx.";
-            }
+        } else if (empty($_POST["NameCake"])) {
+            $error = "Por favor, insira o título do bolo.";
+            echo $error;
         }
-
-
-        if (empty($_POST["NameCake"])) {
-            $name_err = "Por favor, insira o título do bolo.";
-        } else {
-            $name = test_input($_POST["NameCake"]);
-        }
-
-
         if (empty($_POST["Description"])) {
-            $description_err = "Por favor, insira a descrição do bolo.";
+            $error = "Por favor, insira a descrição do bolo.";
+            echo $error;
+        } else if (empty($_POST["Weight"])) {
+            $error = "Por favor, insira o peso do bolo.";
+            echo $error;
         } else {
-            $description = test_input($_POST["Description"]);
-        }
-
-
-        if (empty($_POST["Weight"])) {
-            $weight_err = "Por favor, insira o peso do bolo.";
-        } else {
-            $weight = test_input($_POST["Weight"]);
-            if (!preg_match("/^\d+(\.\d{2})?$/", $weight)) {
-                $weight_err = "O peso deve ser um número no formato xx,xx.";
-            }
-        }
-
-        if (empty($image_err) && empty($price_err) && empty($name_err) && empty($description_err) && empty($weight_err)) {
-            registerCake($image, $price, $name, $description, $weight);
+            echo "Bolo cadastrado com sucesso!";
+            sleep(2);
+            header("Location: ../cadastrarBolo.php");
         }
     }
-}
-
-function test_input($data)
-{
-    $data = trim($data);
-    $data = stripslashes($data);
-    $data = htmlspecialchars($data);
-    return $data;
-}
-
-function registerCake($image, $price, $name, $description, $weight)
-{
 }
 ?>
--- functions/sendMessage.php
+++ functions/sendMessage.php
@@ -2,32 +2,33 @@
 
 // $name = $email = $subject = $message = "";
 
-// if (isset($_POST['submit'])) {
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $name = trim($_POST['name'], FILTER_SANITIZE_STRING);
-    $email = trim($_POST['email'], FILTER_SANITIZE_EMAIL);
-    $subject = trim($_POST['subject'], FILTER_SANITIZE_STRING);
-    $message = trim($_POST['message']);
+    if (isset($_POST['submit'])) {
+        $name = trim($_POST['name'], FILTER_SANITIZE_STRING);
+        $email = trim($_POST['email'], FILTER_SANITIZE_EMAIL);
+        $subject = trim($_POST['subject'], FILTER_SANITIZE_STRING);
+        $message = trim($_POST['message']);
 
-    if (empty($name) || empty($email) || empty($subject) || empty($message)) {
-        $error = "Por favor, preencha todos os campos do formulário.";
-        echo $error;
-    } else {
-        echo "Mensagem enviada com sucesso!";
-        sleep(2);
-        header("Location: ../contato.php");
+        if (empty($name) || empty($email) || empty($subject) || empty($message)) {
+            $error = "Por favor, preencha todos os campos do formulário.";
+            echo $error;
+        } else {
+            echo "Mensagem enviada com sucesso!";
+            sleep(2);
+            header("Location: ../contato.php");
+        }
+
+        $mailTo = "karen.pedrozo@hotmail.com";
+        $headers = "From: " . $email . "\r\n";
+        $txt = "You have recived an email from" . $name . ".\n\n" . $message;
+
+        mail($mailTo, $subject, $txt, $email);
+
+        // if (mail($mailTo, $subject, $txt, $email) == true) {
+        //     echo "Mensagem enviada com sucesso!";
+        // } else {
+        //     echo "Erro ao enviar mensagem. Tente novamente.";
+        // }
     }
-
-    $mailTo = "karen.pedrozo@hotmail.com";
-    $headers = "From: " . $email . "\r\n";
-    $txt = "You have recived an email from" . $name . ".\n\n" . $message;
-
-    mail($mailTo, $subject, $txt, $email);
-
-    // if (mail($mailTo, $subject, $txt, $email) == true) {
-    //     echo "Mensagem enviada com sucesso!";
-    // } else {
-    //     echo "Erro ao enviar mensagem. Tente novamente.";
-    // }
 }
 ?>
